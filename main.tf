de6fff16ba6a57c97034efa7f5fb6fc252dfcf73
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-06-19 17:22:53 +0200

    Change EC2 instance to t4g.small

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ data "aws_ami" "amazon_linux_arm" {
 
 resource "aws_instance" "chartmuseum" {
   ami                         = data.aws_ami.amazon_linux_arm.id
-  instance_type               = "t3.small"
+  instance_type               = "t4g.small"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
